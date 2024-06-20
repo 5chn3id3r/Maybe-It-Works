e9ed056e4625c94b7ad023b88368c1005683855c
Solp1 <114348118+Solp1@users.noreply.github.com>
2024-06-20 21:26:41 +0200

    Creator and destructor

--- src/neurone.cpp
+++ src/neurone.cpp
@@ -1,17 +1,42 @@
 #include "neurone.hpp"
 
-float sigmoid(float x) 
+float sigmoid(float x)
 {
     return 1.f / (1.f + exp(-x));
 }
 
-/* TODO : CreateNeurone(float* weights, unsigned int n_inputs, unsigned int n_outputs)
-a function which allocates memory for neurones and put given weights in parameter
-*/
+Neurone *CreateNeurone(float *weights, unsigned int n_inputs, unsigned int n_outputs)
+{
+    if (n_inputs <= 0 || n_outputs <= 0 || weights == NULL)
+    {
+        printf("Problem in the variable give\n");
+        return nullptr;
+    }
+    Neurone *neurone = (Neurone *)calloc(1, sizeof(Neurone));
+    neurone->n_inputs = n_inputs;
+    neurone->n_outputs = n_outputs;
+    neurone->weights = (float *)calloc(n_inputs, sizeof(float));
+    neurone->weights = weights;
+    neurone->inputs = (float *)calloc(n_inputs, sizeof(float));
+    neurone->inputs = nullptr;
+    neurone->outputs = (float *)calloc(n_outputs, sizeof(float));
+    neurone->outputs = nullptr;
 
-/* TODO : DeleteNeurone(Neurone* neurone)
-a function which frees memory allocated for neurone
-*/
+    return neurone;
+}
+
+void DeleteNeurone(Neurone *neurone)
+{
+    if (neurone == nullptr)
+    {
+        printf("Problem with the neurone given\n");
+        return;
+    }
+    free(neurone->inputs);
+    free(neurone->outputs);
+    free(neurone->weights);
+    free(neurone);
+}
 
 /* TODO : ComputeNeurone(Neurone* neurone)
 a function which computes the output of the neurone
