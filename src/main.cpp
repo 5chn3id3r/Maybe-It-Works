f60187cfa4924c6d3a46bd974b720002e2b86015
N1coc4colA <n1coc4cola@gmail.com>
2024-06-20 22:14:40 +0200

    Lil test for config reading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include "configuration.hpp"
+
 int main()
 {
-	printf("Hello, World!\n");
+	Configuration *config = readConfig("~/Documents/GitHub/Maybe-It-Works/src/TestConfig.txt");
+	if (!config) {
+		return 0;
+	}
+
+	std::cout << config->inputWeightFp << std::endl;
+	std::cout << config->linksFp << std::endl;
+	std::cout << config->neuronesCount << std::endl;
+	std::cout << config->typeFile << std::endl;
+
+	DeleteConfiguration(config);
 	return 0;
 }
